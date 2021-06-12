2969555b4b0510acb05a06bc07c8075ad3cb6af5
EripeLeet <85364143+EripeLeet@users.noreply.github.com>
2021-06-12 17:16:05 +0200

    Update Utils.java

--- src/main/java/me/eripe/trees/utils/Utils.java
+++ src/main/java/me/eripe/trees/utils/Utils.java
@@ -21,20 +21,6 @@ public class Utils {
         return list;
     }
 
-    public static String fixColor(String text){
-        if(text == null || text.isEmpty()) return "";
-        return ChatColor.translateAlternateColorCodes('&', text
-                .replace("%>", "\u00bb")
-                .replace("%O", "\u25cf"));
-    }
-
-    public static List<String> fixColor(List<String> list) {
-        if (list == null || list.isEmpty()) return new ArrayList<>();
-        for(int i = 0; i < list.size(); i++) list.set(i, fixColor(list.get(i)));
-        return list;
-    }
-
-
     public static boolean sendMessage(CommandSender sender, String message){
         if(sender == null || message == null || message.isEmpty()) return false;
         sender.sendMessage(fixColor(message));
